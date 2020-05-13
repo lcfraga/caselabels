cf0a92e71f92d992e7eb11cd53fc2a0a82ceb4a7
Leonardo C. Fraga <lcfraga@gmail.com>
2020-05-13 03:37:10 +0100

    Added CORS middleware to backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
