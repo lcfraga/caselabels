3aaa2a074477e3de5240bae86f02ab2ff47390f8
Leonardo C. Fraga <lcfraga@gmail.com>
2020-07-08 03:00:44 +0100

    [docker] Added id field to case and user mongo initial data

--- docker/mongo/cases.js
+++ docker/mongo/cases.js
@@ -1,24 +1,29 @@
 db.cases.save({
+  id: 'ckccmq2ig00005t236zf0aaq6',
   content:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis. Ipsum nunc aliquet bibendum enim facilisis. Cras tincidunt lobortis feugiat vivamus at. Vitae turpis massa sed elementum tempus egestas. Aenean vel elit scelerisque mauris pellentesque pulvinar. Et egestas quis ipsum suspendisse ultrices gravida dictum. Et ultrices neque ornare aenean euismod elementum. Ac tortor dignissim convallis aenean et. Adipiscing commodo elit at imperdiet dui. Gravida rutrum quisque non tellus.',
 });
 
 db.cases.save({
+  id: 'ckccmqgpl00015t2352jy0has',
   content:
     'At imperdiet dui accumsan sit amet nulla facilisi. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Sed velit dignissim sodales ut eu sem. Ut venenatis tellus in metus vulputate eu scelerisque. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Fermentum iaculis eu non diam. Amet consectetur adipiscing elit pellentesque habitant. Vulputate enim nulla aliquet porttitor lacus. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. At lectus urna duis convallis convallis tellus id interdum. Velit aliquet sagittis id consectetur purus ut. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Aliquam sem et tortor consequat id. Congue mauris rhoncus aenean vel elit scelerisque. Aliquam eleifend mi in nulla posuere sollicitudin aliquam. Arcu cursus vitae congue mauris. Ac odio tempor orci dapibus ultrices. Pretium fusce id velit ut tortor.',
 });
 
 db.cases.save({
+  id: 'ckccmqp8h00025t23fpnocge3',
   content:
     'Et leo duis ut diam. Porttitor massa id neque aliquam. Tincidunt arcu non sodales neque sodales. Sit amet purus gravida quis blandit turpis cursus in hac. Tincidunt eget nullam non nisi est sit amet facilisis magna. Est velit egestas dui id ornare arcu odio. Quis vel eros donec ac odio tempor orci. Est placerat in egestas erat imperdiet. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Erat velit scelerisque in dictum non consectetur. Diam ut venenatis tellus in metus vulputate eu scelerisque. Amet justo donec enim diam vulputate ut pharetra. Leo integer malesuada nunc vel risus commodo viverra. Faucibus turpis in eu mi. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Libero nunc consequat interdum varius sit amet mattis. Id aliquet lectus proin nibh nisl condimentum. Amet commodo nulla facilisi nullam vehicula. Lorem ipsum dolor sit amet consectetur.',
 });
 
 db.cases.save({
+  id: 'ckccmqtqo00035t23aqnzf04v',
   content:
     'Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Duis at consectetur lorem donec massa sapien faucibus et molestie. Venenatis tellus in metus vulputate eu scelerisque felis. Bibendum at varius vel pharetra vel turpis nunc. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Eu scelerisque felis imperdiet proin fermentum leo. Sit amet aliquam id diam maecenas. Tellus elementum sagittis vitae et leo duis ut diam. Euismod lacinia at quis risus sed vulputate. Sit amet mattis vulputate enim nulla aliquet porttitor. Nisi lacus sed viverra tellus in hac habitasse. Neque volutpat ac tincidunt vitae semper quis lectus.'
 });
 
 db.cases.save({
+  id: 'ckccmqy6c00045t230giv0wp0',
   content:
     'Magna fermentum iaculis eu non diam. Quis viverra nibh cras pulvinar mattis nunc sed. Est sit amet facilisis magna etiam tempor. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Leo in vitae turpis massa sed elementum. Velit laoreet id donec ultrices tincidunt. At in tellus integer feugiat scelerisque varius morbi enim nunc. Interdum velit laoreet id donec. Velit scelerisque in dictum non consectetur. Senectus et netus et malesuada fames ac turpis egestas. Risus nec feugiat in fermentum posuere urna. Nam libero justo laoreet sit amet cursus sit amet dictum. Pharetra convallis posuere morbi leo urna molestie at elementum. Odio pellentesque diam volutpat commodo sed. Lorem ipsum dolor sit amet consectetur adipiscing. Facilisis magna etiam tempor orci eu lobortis elementum nibh.'
 });
--- docker/mongo/users.js
+++ docker/mongo/users.js
@@ -1,10 +1,12 @@
 db.users.save({
+  id: 'ckccmtheo000009231y5gb3zn',
   name: 'J. Silver',
   email: 'jsilver@jsilver.com',
   password: '$2a$10$BGEeG/b5TaPzpisomLfQIewfXh3T6p3EARkyN/iCoba.EW053Zk6K', // password
 });
 
 db.users.save({
+  id: 'ckccmts5n00010923fs8p2r44',
   name: 'H. Stevens',
   email: 'hstevens@hstevens.com',
   password: '$2a$10$amDIdV4tv0maRxCmu4c/xeuvu1zooX1iI9f4lJX.nXhpT6QZFH4RK', // 12345678
